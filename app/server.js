b73853dfe2a07290ffb750e34ed238bc30f516ba
eddsonblanco <eddsonrbr@gmail.com>
2019-06-19 00:38:47 -0700

    home.html ready

--- app/server.js
+++ app/server.js
@@ -11,8 +11,8 @@ app.use(express.static("./app/images"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
