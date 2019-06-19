57854e6c5ea0f0852ae02d0aa79ca107e9fba766
eddsonblanco <eddsonrbr@gmail.com>
2019-06-18 23:53:08 -0700

    server.js

---
+++ app/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+app.use(express.static("./app/images"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
